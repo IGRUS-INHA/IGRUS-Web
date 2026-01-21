a0a12a8b89eb2bf9882bda26e348537c516cabb2
won005 <won_8014@naver.com>
2026-01-21 19:31:47 +0900

    SpringBoot 초기세팅

---
+++ backend/src/test/java/igrus/web/IgrusWebApplicationTests.java
@@ -0,0 +1,13 @@
+package igrus.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrusWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
